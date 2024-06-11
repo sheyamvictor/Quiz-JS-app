34f09e795ae61ab64ce9cec369f08e3b03e760ef
sheyamvictor <sheyamjoseph@gmail.com>
2024-06-11 18:39:37 +0530

    updated files  and adding style.css

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
-const ttlQuestionCount=8;
+const ttlQuestionCount = 10;
 
-const userdata ={};
+const userdata = {};
 
-let allQuestions=[];
+let allQuestions = [];
 
-let selectQuestions=[];
+let selectQuestions = [];
 
-let userSelectAns=[];
+let userSelectAns = [];
 
-let currentQuestionIdx=0;
+let currentQuestionIdx = 0;
 
-let mark=0
+let mark = 0
 
-let totalTime=35
+let totalTime = 100
 
-//Timer
+// Timer
 
-const formatTimer=(duration)=>{
-let seconds =Math.floor(duration % 60);
-let minutes = Math.floor((duration / 60) %60);
-let hours = Math.floor(duration/(60*60));
+const formatTimer = (duration) => {
+    let seconds = Math.floor(duration % 60);
+    let minutes = Math.floor((duration / 60) % 60);
 
-return `${string(hours).padStart(2,0)}: ${string(minutes).padStart(2,0)} :${string(seconds).padStart(2,0)}`
+    return `${String(minutes).padStart(2, 0)} :${String(seconds).padStart(2, 0)}`
 
 
 }
 
-
-
-const showTimer=()=>{
+const showTimer = () => {
 
 
     const timer = document.getElementById("timer");
-    timer.innerText=totalTime;
+    timer.innerText = formatTimer(totalTime);
 
 
-let timerInterval=setInterval(()=>{
-    totalTime--;
-    timer.innerText=totalTime;
-    if(totalTime==0){
-        clearInterval(timerInterval)
-        loadingPage("Result.html");
-    }
+    let timerInterval = setInterval(() => {
+        totalTime--;
+        timer.innerText = formatTimer(totalTime);
+        if (totalTime == 0) {
+            clearInterval(timerInterval)
+            loadingPage("Result.html");
+        }
 
-},1000)
+    }, 1000)
 
 }
 
@@ -51,128 +48,155 @@ let timerInterval=setInterval(()=>{
 
 // show answer
 
- const showAnswer=()=>{
+const showAnswer = () => {
 
     const answerContainer = document.getElementById("answerContainer")
-   
-    answerContainer.innerHTML = selectQuestions.map((question , qno) => `
+
+    answerContainer.innerHTML = selectQuestions.map((question, qno) => `
     
     <div>
     
-    <p> Question ${ qno+1 }:${question.question}  </p>   
+    <p> Question ${qno + 1}:${question.question}  </p>   
     
-    ${question.answers.map((ans,ansIdx)=>{ 
-let color = "black";
+    ${question.answers.map((ans, ansIdx) => {
+        let color = "black";
 
-if(question.correct == ansIdx){
-    color ="green";
-}else if (userSelectAns[qno]==ansIdx){
-    color ="red";
-}
+        if (question.correct == ansIdx) {
+            color = "green";
+        } else if (userSelectAns[qno] == ansIdx) {
+            color = "red";
+        }
 
         return `
         <p style ="color:${color}"> ${ans} </p>
         
         `;
-    }) .join("")}
+    }).join("")}
     
     </div>
     
     `
-)
-.join("");
+    )
+        .join("");
 
- };
+};
 
 //select RandomQuestions
-const selectRandomQuestion= () =>{
-let suffledQuestions=allQuestions.sort(()=>{
-    return 5 - Math.floor(Math.random()*10)
-    
-})
+const selectRandomQuestion = () => {
+    let suffledQuestions = allQuestions.sort(() => {
+        return 5 - Math.floor(Math.random() * 10)
 
-selectQuestions=suffledQuestions.slice(0, ttlQuestionCount);
+    })
+
+    selectQuestions = suffledQuestions.slice(0, ttlQuestionCount);
 
 };
 //show Question
-const showQuestion = () =>{
-const currentQuestion = selectQuestions[currentQuestionIdx];
+const showQuestion = () => {
+    const currentQuestion = selectQuestions[currentQuestionIdx];
 
-const questionContainer = document.getElementById("questioncontainer");
+    const questionContainer = document.getElementById("questioncontainer");
 
-questionContainer.innerHTML = `
+    questionContainer.innerHTML = `
         <h4>Question ${currentQuestionIdx + 1}: ${currentQuestion.question}</h4>
         ${currentQuestion.answers.map((ans, idx) => {
-          return `
+        return `
           <div>
-          <label>
-          <input type="radio" name="answer" value="${idx}">${ans}</label></div>`;
-        }).join("")}
-    `;
+          
+           <label ><input  type="radio" name="answer"value="${idx}" ${userSelectAns[currentQuestionIdx] == idx ? 'checked' : ''}>${ans}</label></div>`;
+
+
+    }).join("")}
+`;
+    if (currentQuestionIdx == 0) {
+        document.getElementById("pre").style.display = "none"
+
+    }
+
+    else {
+        document.getElementById("pre").style.display = "inline"
+    }
+
+
+
 };
 
+
+
+
 // click submit loading next page
-const initiasePage=(path)=>{
-    if(path==="First.html"){
-        document.getElementById("userform").addEventListener("submit",(e)=>{
+const initiasePage = (path) => {
+    if (path === "First.html") {
+        document.getElementById("userform").addEventListener("submit", (e) => {
             e.preventDefault();
             let userName = document.getElementById("uname").value
             let userMail = document.getElementById("umail").value
 
-            userdata.uname=userName;
-            userdata.umail=userMail;
+            userdata.uname = userName;
+            userdata.umail = userMail;
 
             loadingPage("Intro.html");
         });
     }
-    else if(path==="Intro.html"){
-        document.getElementById("start").addEventListener("click",()=>{
+    else if (path === "Intro.html") {
+        document.getElementById("start").addEventListener("click", () => {
             loadingPage("Quiz.html")
         });
     }
-else if(path==="Quiz.html"){
-showTimer();
-    showQuestion();
+    else if (path === "Quiz.html") {
+        showTimer();
+        showQuestion();
 
-    document.getElementById("pre").addEventListener("click" ,()=>{
-        let selectAnswer = document.querySelector('input[name="answer"]:checked')
-        if( selectAnswer){
-            userSelectAns[currentQuestionIdx]=selectAnswer.value
+        document.getElementById("pre").addEventListener("click", () => {
+            let selectAnswer = document.querySelector('input[name="answer"]:checked')
+            // if( selectAnswer.value == selectQuestions[currentQuestionIdx].correct){
+            if (selectAnswer) {
+                userSelectAns[currentQuestionIdx] = selectAnswer.value;
+            }
+
+            currentQuestionIdx--;
+
+            showQuestion()
+
+
+
+        })
+
+        document.getElementById("next").addEventListener("click", () => {
+
+            let selectAnswer = document.querySelector('input[name="answer"]:checked')
+
+            if (selectAnswer.value == selectQuestions[currentQuestionIdx].correct) {
+
+                mark++;
+            }
+
+            userSelectAns[currentQuestionIdx] = selectAnswer.value
+
+            currentQuestionIdx++;
+            if (currentQuestionIdx < ttlQuestionCount)
+
+                showQuestion();
+            else loadingPage("Result.html");
+        })
+
+    }
+
+     if (path =="Result.html") {
+        document.getElementById("mark").textContent = ` Hi..😊${userdata.uname} your score is ${mark} / ${ttlQuestionCount}`
         
-        }
-        currentQuestionIdx--;
+        document.getElementById("Call").addEventListener("click",()=>{
+            loadingPage("Answer.html")
+
         
-        showQuestion()                
+        }) 
         
-    })
+    }   
+else if (path=="Answer.html"){
 
-    document.getElementById("next").addEventListener("click" ,()=>{
-
-let selectAnswer = document.querySelector('input[name="answer"]:checked')
-
-if( selectAnswer.value== selectQuestions[currentQuestionIdx].correct){
-
-    mark++;
+    showAnswer();
 }
-
-userSelectAns[currentQuestionIdx]=selectAnswer.value
-
-        currentQuestionIdx++;
-        if(currentQuestionIdx < ttlQuestionCount)
-
-            showQuestion();
-         else loadingPage("Result.html");   
-    })
-
-}
- 
-else if (path == "Result.html"){
-document.getElementById("mark").textContent=`your score is ${mark} / ${ttlQuestionCount}`;
-showAnswer();
-}
-
-};
-
+    };
 
 // connecting to index.html
 const loadingPage = (path) => {
@@ -180,21 +204,21 @@ const loadingPage = (path) => {
         .then((data) => data.text())
         .then((html) => {
             document.getElementById("app").innerHTML = html;
-        initiasePage(path);
+            initiasePage(path);
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.log(err)
         })
 };
 
 //Fetch the Question.json
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     loadingPage("First.html");
     fetch("question.json")
-    .then((data)=> data.json())
-    .then((question)=>{
+        .then((data) => data.json())
+        .then((question) => {
 
-allQuestions=question
-selectRandomQuestion()
-    })
+            allQuestions = question
+            selectRandomQuestion()
+        })
 });
